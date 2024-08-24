8c2d243c9fb902d7185405b86efe061db579580f
MarkMallari16 <mallariftw01@gmail.com>
2024-08-25 00:07:15 +0800

    first vue project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import { reactive, ref } from "vue";
+
+const count = ref(0);
+const message = ref("Hello Vue");
+
+function increment() {
+  count.value++;
+}
+function decrement() {
+  count.value--;
+}
+function reset() {
+  count.value = 0;
+}
+</script>
+
+<template>
+  <div>
+    <h1>Count: {{ count }}</h1>
+
+    <button @click="decrement">Decrement</button>
+    <button @click="reset">Reset</button>
+    <button @click="increment">Increment</button>
+
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
