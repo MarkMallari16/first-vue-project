33a16f1f5dff65b58cfe15116caa4cc334729ff8
MarkMallari16 <mallariftw01@gmail.com>
2024-08-25 00:33:04 +0800

    add form input binding

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@ import { reactive, ref } from "vue";
 
 const count = ref(0);
 const message = ref("Hello Vue");
+const text = ref("First Text");
+const checked = ref(false);
+const studentType = ref(null);
+const checkCourses = ref([]);
 
 function increment() {
   count.value++;
@@ -26,10 +30,29 @@ function reset() {
     <button @click="increment">Increment</button>
 
     <h3>{{ message }}</h3>
+
+    <p>{{ text }}</p>
+
+    <div>Checked Names: {{ checkCourses }}</div>
+    <div>
+      <input type="checkbox" id="bsit" value="BSIT" v-model="checkCourses" />
+      <label for="bsit">BSIT</label>
+
+      <input type="checkbox" id="bscs" value="BSCS" v-model="checkCourses" />
+      <label for="bscs">BSCS</label>
+
+      <input type="checkbox" id="bsce" value="BSCE" v-model="checkCourses" />
+      <label for="bsce">BSCE</label>
+
+      <div>Student Type: {{ studentType }}</div>
+
+      <input type="radio" id="regular" value="Regular" v-model="studentType" />
+      <label for="regular">Regular</label>
+
+      <input type="radio" id="irregular" value="Irregular" v-model="studentType" />
+      <label for="irregular">Irregular</label>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
