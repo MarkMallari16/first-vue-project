d62a6b89be244c7797a4bd82e96db4d77eb2f6b8
MarkMallari16 <mallariftw01@gmail.com>
2024-08-25 14:27:24 +0800

    add checkbox if task is done

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ const count = ref(0);
 let id = 0;
 const newTodo = ref("");
 const todos = ref([
-  { id: id++, text: "Kain" },
-  { id: id++, text: "Tulog" },
+  { id: id++, text: "Kain", done: true },
+  { id: id++, text: "Tulog", done: false },
 ]);
 
 const editingTodoId = ref(null);
@@ -31,6 +31,7 @@ function addTodo() {
   todos.value.push({ id: id++, text: newTodo.value });
   newTodo.value = "";
 }
+
 function removeTodo(id) {
   todos.value = todos.value.filter((todo) => todo.id != id);
 }
@@ -57,7 +58,7 @@ function saveTodo() {
   <div>
     <div class="flex justify-center">
       <div>
-        <h1 class="text-center text-3xl mb-2">Count: {{ count }}</h1>
+        <h1 class="text-center text-3xl my-4">Count: {{ count }}</h1>
         <div class="flex gap-3">
           <button @click="decrement" class="btn btn-error">Decrement</button>
           <button @click="reset" class="btn btn-secondary">Reset</button>
@@ -69,6 +70,7 @@ function saveTodo() {
     <div class="m-20">
       <h1 class="text-4xl text-center mt-20">Simple To-do List</h1>
       <h2 class="text-2xl mb-5">Tasks: {{ todos.length }}</h2>
+
       <form @submit.prevent="addTodo" class="mb-5 flex gap-5">
         <input
           v-model="newTodo"
@@ -83,6 +85,8 @@ function saveTodo() {
           v-for="todo in todos"
           :key="todo.id"
         >
+          <input type="checkbox" v-model="todo.done" class="checkbox"/>
+
           <div v-if="editingTodoId === todo.id" class="flex gap-2">
             <input
               v-model="editingTodoText"
@@ -91,7 +95,7 @@ function saveTodo() {
             />
             <button @click="saveTodo" class="btn btn-primary">Save</button>
           </div>
-          <p v-else class="font-medium">{{ todo.text }}</p>
+          <p v-else class="font-medium" :class="{'line-through': todo.done, 'text-green-500': todo.done }">{{ todo.text }}</p>
           <div class="flex gap-2">
             <button @click="editTodo(todo.id)" class="btn btn-secondary">Edit</button>
             <button @click="removeTodo(todo.id)" class="btn btn-error">Delete</button>
