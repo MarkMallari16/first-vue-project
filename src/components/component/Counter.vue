a965e3202c3bb2fb220b063c97750ab9fce52b37
MarkMallari16 <mallariftw01@gmail.com>
2024-08-26 10:57:41 +0800

    separate the code of counter and todolist to make it reusable and clean

---
+++ src/components/component/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-center">
+    <div>
+      <h1 class="text-center text-3xl my-4">Count: {{ count }}</h1>
+      <div class="flex gap-3">
+        <button @click="decrement" class="btn btn-error">Decrement</button>
+        <button @click="reset" class="btn btn-secondary">Reset</button>
+        <button @click="increment" class="btn btn-primary">Increment</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+const count = ref(0);
+function increment() {
+  count.value++;
+}
+function decrement() {
+  if (count.value > 0) {
+    count.value--;
+  }
+}
+function reset() {
+  count.value = 0;
+}
+</script>
